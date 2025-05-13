ff568dcd8b809930d39e5dce8185090d6408bd45
José Luiz <jlgussani@gmail.com>
2025-05-12 21:54:59 -0300

    data correção

---
+++ data/insert_cliente.sql
@@ -0,0 +1,14 @@
+-- Comando insert para inserir 10 clientes na tabela cliente contendo nome, cpf, email, telefone e data de nascimento.
+INSERT INTO cliente (nome, cpf, email, telefone, data_nascimento) VALUES
+
+('João Silva', '12345678901', 'joaosilva@gmail.com', '11987654321', '1990-01-01'),
+('Maria Oliveira', '23456789012', 'mariaoliveira@gmail.com', '11987654322', '1985-02-02'),
+('Ana Costa', '45678901234', 'anacosta@gmail.com', '11987654324', '1988-04-04'),
+('Carlos Lima', '56789012345', 'carloslima@gmail.com', '11987654325', '1995-05-05'),
+('Fernanda Souza', '67890123456', 'fernandasouza@gmail.com', '11987654326', '1993-06-06'),
+('Rafael Almeida', '78901234567', 'rafaelalmeida@gmail.com', '11987654327', '1991-07-07'),
+('Juliana Pereira', '89012345678', 'julianapereira@gmail.com', '11987654328', '1987-08-08'),
+('Bruno Rocha', '90123456789', 'brunorocha@gmail.com', '11987654329', '1994-09-09'),
+('Patricia Mendes', '01234567890', 'patriciamendes@gmail.com', '11987654330', '1989-10-10'),
+('Gustavo Nunes', '12345098765', 'gustavonunes@gmail.com', '11987654331', '1996-11-11'),
+('Camila Ribeiro', '23456109876', 'camilaribeiro@gmail.com', '11987654332', '1990-12-12');
